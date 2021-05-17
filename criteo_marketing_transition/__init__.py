3385e9e578c02ec72673b19fcd1d67d80b3d20ee
je.buisson <je.buisson@criteo.com>
2021-05-17 15:34:13 +0200

    Bumb version to v1.0.2

--- criteo_marketing_transition/__init__.py
+++ criteo_marketing_transition/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 # import apis into sdk package
 from criteo_marketing_transition.api.advertiser_api import AdvertiserApi
